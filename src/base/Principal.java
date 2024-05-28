90beafd2474ac69478d87fe919d1ed12f3f98c16
EvaChios <156311835+EvaChios@users.noreply.github.com>
2024-05-28 20:40:47 +0200

    varios mensajes

--- src/base/Principal.java
+++ src/base/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 	System.out.println("Hola Naaat");
+	System.out.println("Ya hemos conseguido avanzar en tema de repositorio que maravilla!!");
 	}
 
 }
